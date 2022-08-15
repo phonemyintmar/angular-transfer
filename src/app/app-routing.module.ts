d35a15618fbbd6f6832350f8158dc04a5d9a4cb6
Phone Myint <phonemyintmar@mytel.com.mm>
2022-08-15 14:03:52 +0630

    workflow

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,17 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'hello-world' },
   { path: 'hello-world', component: HelloWorldComponent },
   { path: 'transfer-money', component: TransferMoneyComponent },
-  { path: 'transaction', loadChildren: () => import('./transaction/transaction.module').then(m => m.TransactionModule) },
+  {
+    path: 'transaction',
+    loadChildren: () =>
+      import('./transaction/transaction.module').then(
+        (m) => m.TransactionModule
+      ),
+  },
+  {
+    path: 'test',
+    loadChildren: () => import('./test/test.module').then((m) => m.TestModule),
+  },
   { path: '**', component: FourOfourComponent },
 ];
 
--- src/app/transfer-money/transfer-money.service.ts
+++ src/app/transfer-money/transfer-money.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
